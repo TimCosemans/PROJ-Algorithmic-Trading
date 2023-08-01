85a544a4eb688ae90b057603c635848e818fcbfc
Tim Cosemans <tim_cosemans@hotmail.com>
2023-08-01 09:23:39 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project for the course MLOps Zoomcamp',
+    author='Tim Cosemans',
+    license='',
+)
